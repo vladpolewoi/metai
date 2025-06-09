a27f5ea84ce77d2ae5230ed5fb421fa7751886ab
vlad polevoi <vlad.polewoi1@gmail.com>
2025-06-09 22:26:18 +0300

    04 Auth UI

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -15,6 +15,10 @@ import { Input } from '@/components/ui/input';
 import { Alert, AlertTitle } from '@/components/ui/alert';
 import { OctagonAlertIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { authClient } from '@/lib/auth-client';
 
 const formSchema = z.object({
   email: z.string().email(),
@@ -22,6 +26,10 @@ const formSchema = z.object({
 });
 
 export const SignInView = () => {
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+  const [pending, setPending] = useState<boolean>(false);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -30,12 +38,31 @@ export const SignInView = () => {
     },
   });
 
+  const onSubmit = async (data: z.infer<typeof formSchema>) => {
+    setError(null);
+    setPending(true);
+
+    await authClient.signIn.email(
+      { email: data.email, password: data.password },
+      {
+        onSuccess: () => {
+          setPending(false);
+          router.push('/');
+        },
+        onError: ({ error }) => {
+          setPending(false);
+          setError(error.message);
+        },
+      },
+    );
+  };
+
   return (
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
           <Form {...form}>
-            <form className="p-6 md:p-8">
+            <form className="p-6 md:p-8" onSubmit={form.handleSubmit(onSubmit)}>
               <div className="flex flex-col gap-6">
                 <div className="flex flex-col items-center text-center">
                   <h1 className="text-2xl font-bold">Welcome Back</h1>
@@ -71,13 +98,13 @@ export const SignInView = () => {
                     )}
                   />
                 </div>
-                {true && (
+                {!!error && (
                   <Alert className="bg-destructive/10 border-none">
                     <OctagonAlertIcon className="h-4 w-4 !text-destructive"></OctagonAlertIcon>
-                    <AlertTitle>Error</AlertTitle>
+                    <AlertTitle>{error}</AlertTitle>
                   </Alert>
                 )}
-                <Button type="submit" className="w-full">
+                <Button type="submit" className="w-full" disabled={pending}>
                   Sign In
                 </Button>
                 <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
@@ -85,15 +112,31 @@ export const SignInView = () => {
                     Or continue with
                   </span>
                 </div>
+                <div className="grid grid-cols-2 gap-4">
+                  <Button variant={'outline'} type="button" className="w-full" disabled={pending}>
+                    Google
+                  </Button>
+                  <Button variant={'outline'} type="button" className="w-full" disabled={pending}>
+                    Github
+                  </Button>
+                </div>
+                <div className="text-center text-sm">
+                  Don&apos;t have an account? <Link href="/sign-up">Sign up</Link>
+                </div>
               </div>
             </form>
           </Form>
-          <div className="bg-radial from-green-500 to-green-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
+          <div className="bg-radial from-sky-500 to-sky-900 relative hidden md:flex flex-col gap-y-4 items-center justify-center">
             <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]" />
             <p className="text-2xl font-semibold text-white">Met.AI</p>
           </div>
         </CardContent>
       </Card>
+
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a> and{' '}
+        <a href="#">Privacy Policy</a>
+      </div>
     </div>
   );
 };
